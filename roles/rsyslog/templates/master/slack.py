236b0ff9087a46204b9d676b621cad3af8488f11
jinqi <jinqiqiqi@gmail.com>
2020-06-10 21:01:25 +0800

    comment requests call to api server.

--- roles/rsyslog/templates/master/slack.py
+++ roles/rsyslog/templates/master/slack.py
@@ -3,12 +3,12 @@
 import sys
 import requests
 
-data_url="http://192.168.99.1:8000/geoip/pdt/"
+# data_url="http://192.168.99.1:8000/geoip/pdt/"
 while True:
     for line in sys.stdin:
         msg = line.strip()
         if not msg:
             break
-        dict_data = {"data":  msg}
-        requests.post(data_url, data=dict_data)
+        # dict_data = {"data":  msg}
+        # requests.post(data_url, data=dict_data)
 
